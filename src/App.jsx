f0a02e76abe45d78b2d8fa4ef121d324e07dde60
Tronjan <workiniranjan10@gmail.com>
2024-01-25 00:54:35 +0000

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { About } from './components/About/About'
 import { Contact } from './components/Contact/Contact'
 import { Experience } from './components/Experience/Experience'
 import { Hero } from './components/Hero/Hero'
-import { Navbar } from './components/Navbar/navbar'
+import { Navbar } from './components/Navbar/Navbar'
 import { Projects } from './components/Projects/Projects'
 function App() {
 
