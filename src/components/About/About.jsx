8364eec3a9781021c4641305ebc3770d62240f51
Tronjan <workiniranjan10@gmail.com>
2024-02-27 12:32:19 +0000

    lottie icons in about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { getImageUrl } from '../../utils'
 import styles from "./About.module.css";
+import graph from "../../../assets/AnimatedSkills/graph.json";
+import coder from "../../../assets/AnimatedSkills/coder.json";
+import Lottie from 'lottie-react';
 
 export const About = () => {
   return <section className={styles.container} id="about"> 
@@ -13,7 +16,7 @@ export const About = () => {
         />
         <ul className={styles.aboutItems}>
             <li className={styles.aboutItem}>
-                <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor" />
+                <Lottie loop={true} animationData={graph} className={styles.lottieImage}/>
                 <div className={styles.aboutItemText}>
                     <h3>Business Analyst</h3>
                     <p>I'm pursuing MSc. Business Analytics at University of Birmingham
@@ -22,18 +25,17 @@ export const About = () => {
                 </div>
             </li>
             <li className={styles.aboutItem}>
-                <img src={getImageUrl("about/serverIcon.png")} alt="Server" />
+                <Lottie loop={true} animationData={coder} className={styles.lottieImage}/>
                 <div className={styles.aboutItemText}>
-                    <h3>Software Engineer - Backend Developer</h3>
+                    <h3>Software Engineer</h3>
                     <p>
-                        I have experience developing fast and optimised backend systems 
-                        and APIs. Contributed to healthcare system features, 
-                        showcasing expertise in C# .NET Core, REST, and third part Integration. 
+                        Developed fast and optimised backend systems and APIs.<br/> Contributed to healthcare system features, 
+                        showcasing expertise in C# .NET Core, REST, and third part Integration.
                     </p>
                 </div>
             </li>
             <li className={styles.aboutItem}>
-                <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor" />
+                <Lottie loop={true} animationData={coder} className={styles.lottieImage}/>
                 <div className={styles.aboutItemText}>
                     <h3>Frontend Developer</h3>
                     <p>
